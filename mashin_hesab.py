878f981c3c3d2250f0becba0c9dd5cf93fbed058
zahratorki <97352494+zahratorki@users.noreply.github.com>
2023-09-15 20:43:43 +0430

    first comit

---
+++ mashin_hesab.py
@@ -0,0 +1,18 @@
+
+num1 = float(input("adade aval ro vared kon: "))
+num2 = float(input("adade dowom ro vared kon: "))
+oper = input("amaliat ro vared kon: ")
+
+#print( num1, num2, oper)
+print("natije:")
+
+if oper == "+":
+    print(num1 + num2)
+elif oper == "*":
+    print(num1 * num2)
+elif oper == "-":
+    print(num1 - num2)
+elif oper == "/":
+    print(num1 / num2)
+else:
+    print("motabar nist")
